ab8db250bcdedbae0ffc5ec0dde14ed135a67003
Mike Chamberlain <mike.chamberlain@servian.com.au>
2016-11-23 17:51:56 +1100

    initial commit

---
+++ src/DOMElementWatcher.IE9-10.js
@@ -0,0 +1,5 @@
+// polyfills for IE9-10
+require('../node_modules/webcomponents.js/src/WeakMap/WeakMap');
+require('../node_modules/webcomponents.js/src/MutationObserver/MutationObserver');
+
+require('./DOMElementWatcher');
