4399c1535e494caca0a929258608b8f6245e781d
Mike Chamberlain <mikeachamberlain@gmail.com>
2016-11-23 22:57:43 +1100

    better documentation; fixing NPM packing

--- README.md
+++ README.md
@@ -4,11 +4,34 @@ Watches for DOM elements, invoking a callback as and when they are added to the 
 Uses a [MutationObserver](https://developer.mozilla.org/en/docs/Web/API/MutationObserver) to listen for new elements.
 Polyfilled version available for IE9-10.
 
+## Installation
+
+```
+npm install dom-element-watcher --save
+```
+
+Then add a reference to one of:
+
+```
+./node_modules/dom-element-watcher/DOMElementWatcher.js             # native (~4KB)
+./node_modules/dom-element-watcher/DOMElementWatcher.min.js         # native, minified (<1KB)
+./node_modules/dom-element-watcher/DOMElementWatcher.IE9-10.js      # polyfilled for old IE (~24KB)
+./node_modules/dom-element-watcher/DOMElementWatcher.IE9-10.min.js  # polyfilled for old IE, minified (~7KB)
+```
+
+eg:
+
+```
+<script src="./node_modules/dom-element-watcher/DOMElementWatcher.min.js></script>
+```
+
 ## Example usage
 
 ```
 // create the watcher
 var elementWatcher = new DOMElementWatcher();
+
+// start listening for mutations
 elementWatcher.startWatching();
 
 // for all DIV elements, either existing now or added in the future, set their background color to red
@@ -16,11 +39,35 @@ elementWatcher.when('div', -1, function (element) {
     element.style.background = 'red';
 });
 
+// then perhaps later...
+elementWatcher.stopListening()
+
 ```
 
-## Build
+## API
+
+### `.when(selector, index, callback)`
+
+Observes elements matching the selector with the given index. Executes the callback once for each element.
+If a matching element is already present in the DOM, the callback will be executed immediately.
+
+`selector`: A CSS selector against which to match elements.
+`index`: In the case where multiple elements are matched by the selector, this restricts the match to this array index. Pass in `-1` to match all elements.
+`callback`: The function to call when a matched element is added to the DOM. The callback is passed the matched element as its only argument.
+
+### `.startWatching()`
+
+Starts watching for DOM Mutations. 
+
+### `.stopWatching()`
+
+Stops watching for DOM Mutations.
+
+## Development
 
 ```
+git clone https://github.com/mikechamberlain/dom-element-watcher.git
+cd dom-element-watcher
 npm install
 npm run build
 ```
--- src/DOMElementWatcher.js
+++ src/DOMElementWatcher.js
@@ -1,88 +1,93 @@
-/**
- * Watches for DOM elements, invoking a callback if and when they become available.
- */
-module.exports = function DOMElementWatcher() {
-    var MODIFIED_KEY = '__DOMElementWatcherModified__';
-    var observed = [];
-    var observer;
-
+(function (window) {
     /**
-     * Starts watching for elements.
+     }
+     * Watches for DOM elements, invoking a callback if and when they become available.
      */
-    function startWatching() {
-        observer = observer || new MutationObserver(onDomMutation);
-        observer.observe(document.documentElement, {
-            childList: true,
-            subtree: true
-        });
-    }
-    this.startWatching = startWatching;
+    window.DOMElementWatcher = function () {
+        var MODIFIED_KEY = '__DOMElementWatcherModified__';
+        var observed = [];
+        var observer;
 
-    /**
-     * Stops watching for elements.
-     */
-    function stopWatching() {
-        observer && observer.disconnect();
-    }
-    this.stopWatching = stopWatching;
-
-    /**
-     * Invokes the callback on all elements matching the selector, either now or if they become available in the future.
-     * @selector {string} The CSS selector
-     * @index {number} The zero-based index of the selector results against which to invoke the callback, or -1 for all
-     * selected elements.
-     * @callback {Function} The callback to execute if and when the element becomes available. The callback will be executed
-     * only once for each element per selector per callback.
-     */
-    this.when = function(selector, index, callback) {
-        // store to check against future DOM mutations
-        observed.push({
-            selector: selector,
-            index: index,
-            callback: callback
-        });
-
-        // invoke callback for elements currently in the DOM
-        invokeNow(selector, index, callback);
-    };
-
-    /**
-     * Queries the DOM for given selector / index and invokes the callback if it has not been invoked before for this
-     * element.
-     * @param {string} selector
-     * @param {number} index
-     * @param {Function} callback
-     */
-    function invokeNow(selector, index, callback) {
-        // make sure we do not later observe any mutations as a result of our own changes
-        stopWatching();
-
-        var elements = document.querySelectorAll(selector);
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            // make sure we are matching against the right index
-            if (index >= 0 && index !== i) {
-                continue;
-            }
-            // make sure we invoke the callback on this element only once per selector per callback
-            if (element[MODIFIED_KEY] && element[MODIFIED_KEY][selector] && element[MODIFIED_KEY][selector][callback]) {
-                continue;
-            }
-            element[MODIFIED_KEY] = element[MODIFIED_KEY] || {};
-            element[MODIFIED_KEY][selector] = element[MODIFIED_KEY][selector] || {};
-            element[MODIFIED_KEY][selector][callback] = element[MODIFIED_KEY][selector][callback] || true;
-            callback(element);
+        /**
+         * Starts watching for elements.
+         */
+        function startWatching() {
+            observer = observer || new MutationObserver(onDomMutation);
+            observer.observe(window.document.documentElement, {
+                childList: true,
+                subtree: true
+            });
         }
-        startWatching();
-    }
 
-    /**
-     * Called upon any DOM mutation.  Iterates through our watched selectors and invokes the callback.
-     */
-    function onDomMutation() {
-        // check the DOM for elements matching a stored selector
-        observed.forEach(function (listener) {
-            invokeNow(listener.selector, listener.index, listener.callback);
-        });
-    }
-};
+        this.startWatching = startWatching;
+
+        /**
+         * Stops watching for elements.
+         */
+        function stopWatching() {
+            observer && observer.disconnect();
+        }
+
+        this.stopWatching = stopWatching;
+
+        /**
+         * Invokes the callback on all elements matching the selector, either now or if they become available in the future.
+         * @selector {string} The CSS selector
+         * @index {number} The zero-based index of the selector results against which to invoke the callback, or -1 for all
+         * selected elements.
+         * @callback {Function} The callback to execute if and when the element becomes available. The callback will be executed
+         * only once for each element per selector per callback.
+         */
+        this.when = function (selector, index, callback) {
+            // store to check against future DOM mutations
+            observed.push({
+                selector: selector,
+                index: index,
+                callback: callback
+            });
+
+            // invoke callback for elements currently in the DOM
+            invokeNow(selector, index, callback);
+        };
+
+        /**
+         * Queries the DOM for given selector / index and invokes the callback if it has not been invoked before for this
+         * element.
+         * @param {string} selector
+         * @param {number} index
+         * @param {Function} callback
+         */
+        function invokeNow(selector, index, callback) {
+            // make sure we do not later observe any mutations as a result of our own changes
+            stopWatching();
+
+            var elements = window.document.querySelectorAll(selector);
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                // make sure we are matching against the right index
+                if (index >= 0 && index !== i) {
+                    continue;
+                }
+                // make sure we invoke the callback on this element only once per selector per callback
+                if (element[MODIFIED_KEY] && element[MODIFIED_KEY][selector] && element[MODIFIED_KEY][selector][callback]) {
+                    continue;
+                }
+                element[MODIFIED_KEY] = element[MODIFIED_KEY] || {};
+                element[MODIFIED_KEY][selector] = element[MODIFIED_KEY][selector] || {};
+                element[MODIFIED_KEY][selector][callback] = element[MODIFIED_KEY][selector][callback] || true;
+                callback(element);
+            }
+            startWatching();
+        }
+
+        /**
+         * Called upon any DOM mutation.  Iterates through our watched selectors and invokes the callback.
+         */
+        function onDomMutation() {
+            // check the DOM for elements matching a stored selector
+            observed.forEach(function (listener) {
+                invokeNow(listener.selector, listener.index, listener.callback);
+            });
+        }
+    };
+})(window);
