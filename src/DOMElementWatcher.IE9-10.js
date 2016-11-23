fc4797a585c4449a9a6590bb1af071d3bba26604
Mike Chamberlain <mikeachamberlain@gmail.com>
2016-11-24 03:31:41 +1100

    Update DOMElementWatcher.IE9-10.js

--- src/DOMElementWatcher.IE9-10.js
+++ src/DOMElementWatcher.IE9-10.js
@@ -1,3 +1,5 @@
+// `npm run build` to browserify this file
+
 // polyfills for IE9-10
 require('../node_modules/webcomponents.js/src/WeakMap/WeakMap');
 require('../node_modules/webcomponents.js/src/MutationObserver/MutationObserver');
