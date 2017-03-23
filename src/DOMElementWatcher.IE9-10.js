a5a20b4c14972df566120e9aa0ba1f4a6951c095
Mike Chamberlain <mike.chamberlain@servian.com.au>
2017-03-23 16:22:13 +1100

    pinning polyfill to correct git tag

--- src/DOMElementWatcher.IE9-10.js
+++ src/DOMElementWatcher.IE9-10.js
@@ -1,7 +1,5 @@
 // `npm run build` to browserify this file
 
 // polyfills for IE9-10
-require('../node_modules/webcomponents.js/src/WeakMap/WeakMap');
-require('../node_modules/webcomponents.js/src/MutationObserver/MutationObserver');
-
+require('../node_modules/webcomponents.js/MutationObserver');
 require('./DOMElementWatcher');
